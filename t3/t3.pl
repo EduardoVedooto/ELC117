69340ec8136a9fe534a32c26ab5b5a04c05628d6
Eduardo Rivas Vedooto <48696911+EduardoVedooto@users.noreply.github.com>
2019-05-01 20:36:35 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,12 +1,5 @@
 %Author: Eduardo Rivas Vedooto
 
-repete(0, _, []).
-repete(N, C, L) :-
- N > 0,
- L = [C | T],
- N1 is N - 1,
- repete(N1, C, T).
-
 /*1- Defina um predicado odd(N) que seja verdadeiro se 
 N for um número ímpar. Exemplo de uso:*/
 odd(N) :-
